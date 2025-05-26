45becad191f4b0b465827a5784ec2497f5fba04a
benarivo66 <iz4comed@yahoo.com>
2025-05-26 20:59:25 +0100

    set up and home page

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Roboto, Open_Sans } from 'next/font/google';
+
+export const roboto = Roboto({
+  weight: ['400', '700'], 
+  subsets: ['latin'],
+});//Header
+
+export const openSans = Open_Sans({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});//Text
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        primary: '#2e2c2f',
+        secondary: '#729b79',
+        tertiary: '#f3e8ee',
+      },
+      keyframes: {
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+
+export default config;
